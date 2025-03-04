c70a965e8835b3513dc9752e20a8b84774717015
Fares-TGB <ftognibo@gmail.com>
2025-03-04 05:18:32 +0100

    table ajoutee

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,13 @@ import Cards from "./cards";
 import Faq from "./Faq";
 import Formulaire from "./Formulaire";
 import Header from "./header";
-
+import Table from "./table";
 export default function Home() {
   return (
     <div className="min-h-full  bg-[#FFFCF7] pt-10 pb-96 flex flex-col items-center">
       <Header />
       <Cards />
+      <Table />
       <Faq />
       <Formulaire />
     </div>
